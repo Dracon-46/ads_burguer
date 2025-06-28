12a0f7c3fe0da7fb48fa361bc59ad14a0433393d
nicollylemos <nicollylemosdasilva@gmail.com>
2025-06-28 00:07:38 -0300

    antes de dar bomba

--- TotemPWA/ViewModels/ProductViewModel.cs
+++ TotemPWA/ViewModels/ProductViewModel.cs
@@ -1,15 +1,15 @@
-using Microsoft.AspNetCore.Mvc.Rendering;
-using TotemPWA.Models;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using TotemPWA.Models;
 
-namespace TotemPWA.ViewModels
-{
-    public class ProductViewModel
+    namespace TotemPWA.ViewModels
     {
-        // Remova '= new Product();' ou '= default!;'
-        // Apenas declare a propriedade:
-        public Product Product { get; set; } = null!; // Adicionado '= null!;' para suprimir o warning CS8618 se aparecer.
-                                                       // O Model Binder cuidará da inicialização.
+        public class ProductViewModel
+        {
+            // Remova '= new Product();' ou '= default!;'
+            // Apenas declare a propriedade:
+            public Product Product { get; set; } = null!; // Adicionado '= null!;' para suprimir o warning CS8618 se aparecer.
+                                                        // O Model Binder cuidará da inicialização.
 
-        public List<SelectListItem>? Categories { get; set; }
+            public List<SelectListItem>? Categories { get; set; }
+        }
     }
-}
